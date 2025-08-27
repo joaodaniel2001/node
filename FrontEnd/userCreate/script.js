0aca96a9b1aa1e5ed2fd69b3a7a223413bf7a173
João <joaodaniel2al@gmail.com>
2025-08-27 16:43:48 -0300

    Post

---
+++ FrontEnd/userCreate/script.js
@@ -0,0 +1,33 @@
+
+function cadastroUsuario(event) {
+    event.preventDefault();
+
+    let nome = event.target.nome.value;
+    let idade = event.target.idade.value;
+    let senha = event.target.senha.value;
+
+    fetch('http://localhost:3000/usuarios', {
+
+        method: 'POST',
+
+        headers: {
+
+            'Content-Type': 'application/json'
+
+        },
+
+        body: JSON.stringify({
+            "nome": nome,
+            "idade": idade,
+            "senha": senha
+        })
+
+    })
+
+        .then(response => response.json())
+
+        .then(data => console.log(data))
+
+        .catch(error => console.log(error));
+
+}
